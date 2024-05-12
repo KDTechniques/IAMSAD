5720f152fdc20b22f134b4fb90a4e66f04e32d3b
Mr. Kavinda Dilshan <kdtechniques@gmail.com>
2024-05-12 15:57:14 +0530

    Fixed UI Alignment Issues and Completed Collage Bubble Type

--- IAMSAD/MODELS 💚💚💚/Conversations Tab/CollageBubbleModel.swift
+++ IAMSAD/MODELS 💚💚💚/Conversations Tab/CollageBubbleModel.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct CollageBubbleModel {
     // MARK: - PROPERTIES
-    let imageURLString: String
+    let type: Types
+    let urlString: String
     let msgBubbleObj: MessageBubbleValues.MessageBubbleModel
     
+    enum Types: CaseIterable { case photo, video }
+    
     // MARK: - INITITLAIZER
-    init(imageURLString: String, msgBubbleObj: MessageBubbleValues.MessageBubbleModel) {
-        self.imageURLString = imageURLString
+    init(type: Types, urlString: String, msgBubbleObj: MessageBubbleValues.MessageBubbleModel) {
+        self.type = type
+        self.urlString = urlString
         self.msgBubbleObj = msgBubbleObj
     }
     
@@ -26,7 +30,8 @@ struct CollageBubbleModel {
         
         for _ in 1...Int.random(in: 4...10) {
             let obj: Self = self.init(
-                imageURLString: "https://picsum.photos/id/\(Int.random(in: 100...200))/500",
+                type: .random(),
+                urlString: "https://picsum.photos/id/\(Int.random(in: 100...200))/500",
                 msgBubbleObj: .getRandomMockObject()
             )
             
