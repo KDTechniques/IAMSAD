c1155dcd6fb1915a815a83ec586c07deb1976283
Kavinda Dilshan <kdtechniques@gmail.com>
2024-08-07 13:21:04 +0530

    [Feature] hides top & bottom dividers of a row on `see all avatar collection sheet` when taps on for better ux

--- IAMSAD/VIEWS ❤️❤️❤️/Reusable/Avatar Selection Sheet/See All Avatars Sheet/Scroll Content/SeeAllAvatarSheetScrollView.swift
+++ IAMSAD/VIEWS ❤️❤️❤️/Reusable/Avatar Selection Sheet/See All Avatars Sheet/Scroll Content/SeeAllAvatarSheetScrollView.swift
@@ -16,7 +16,7 @@ struct SeeAllAvatarSheetScrollView: View {
     
     // MARK: - PRIVATE PROPERTIES
     let avatarCollectionsArray: [AvatarCollectionModel] = AvatarCollectionTypes.avatarCollectionsArray
-    var lastCollectionID: String? { avatarCollectionsArray.last?.id }
+    private var lastCollectionID: String? { avatarCollectionsArray.last?.id }
     
     // MARK: - INITIALIZER
     init(selectedCollection: Binding<AvatarCollectionModel?>, showRowBackground: Binding<Bool>) {
@@ -33,7 +33,7 @@ struct SeeAllAvatarSheetScrollView: View {
                         SeeAllAvatarSheetCollectionHeaderView(
                             collectionName: item.collectionName
                         )
-                    
+                        
                         SeeAllAvatarSheetCollectionRowView(collectionName: item.collectionName)
                     }
                     .padding(.horizontal)
@@ -70,13 +70,35 @@ struct SeeAllAvatarSheetScrollView: View {
 // MARK: - EXTENSIONS
 extension SeeAllAvatarSheetScrollView {
     // MARK: - bottomOverlayDivider
+    @ViewBuilder
     private func bottomOverlayDivider(id: String) -> some View {
+        let currentNPreviousIDs: (cID: String, pID: String) = getSelectedNPreviousID()
+        
         Divider()
             .padding(.horizontal)
             .opacity(id == (lastCollectionID ?? "") ? 0 : 1)
+            .opacity(currentNPreviousIDs.cID == id ? 0 : 1)
+            .opacity(currentNPreviousIDs.pID == id ? 0 : 1)
     }
     
     // MARK: - FUNCTIONS
+    
+    // MARK: - getSelectedNPreviousID
+    private func getSelectedNPreviousID() -> (cID: String, pID: String) {
+        guard showRowBackground,
+              let selectedCollectionID: String = selectedCollection?.id,
+              let index: Int = avatarCollectionsArray.firstIndex(where: { $0.id == selectedCollectionID }) else {
+            return ("", "")
+        }
+        
+        var previousCollectionID: String {
+            let previousIndex: Int = index-1
+            
+            return previousIndex >= 0 ? avatarCollectionsArray[previousIndex].id : ""
+        }
+        
+        return (selectedCollectionID, previousCollectionID)
+    }
     
     // MARK: - checkSelectedCollection
     private func checkSelectedCollection(_ item: AvatarCollectionModel) -> Bool {
